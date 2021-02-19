23a713c0e67ddac8a9aef61124fd7b1bdaa3d9cb
Jean Pierre Ortiz Murcia <ortizmurciajeanpierre@gmail.com>
2021-02-19 08:14:48 -0500

    feat: init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+<h1 align="center">React Typescript Template</h1>
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const DotenvWebpackPlugin = require('dotenv-webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+
+const mode =
+  process.env.NODE_ENV === 'production' ? 'production' : 'development'
+
+module.exports = {
+  entry: {
+    index: path.join(__dirname, 'src', 'index.js'),
+  },
+  output: {
+    publicPath: '/',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  mode,
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|mp3|svg)$/i,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html'),
+    }),
+    new DotenvWebpackPlugin(),
+    new MiniCSSExtractPlugin(),
+  ],
+  devServer: {
+    index: path.join(__dirname, 'dist', 'index.html'),
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
+}
